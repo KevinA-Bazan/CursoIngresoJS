b51467ff2c707740321a62106e2317b746426eb1
KevinA-Bazan <60743509+KevinA-Bazan@users.noreply.github.com>
2020-02-06 11:13:06 -0300

    Ejercicio uno
    -Uso de Alert
    -Uso de Comentarios

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,21 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
+	/*
+
+
+	*/
 function mostrar()
 {
-	alert("ok");
+	var PrimerPrecio;//Segunda variable con , No preferible
+	var SegundoPrecio;
+	var Apellido="nadie";
+
+	Apellido="Perez";
+	alert(Apellido);
+	alert(PrimerPrecio);//Aparece Indefinido
+	//Es un valor literal si esta entre ""
+	alert("Tal cual");//Literal
+	alert(Apellido);//Variable
+	alert();//Sale vacio
+	alert(preciopri);//No existe
 }
 
